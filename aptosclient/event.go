8ea7352fe14d178757fd9dd6bf3117373f09fe65
xiang-xx <xiang9311@gmail.com>
2022-09-19 10:22:40 +0800

    feat(client): remove deprecated method

--- aptosclient/event.go
+++ aptosclient/event.go
@@ -7,17 +7,21 @@ import (
 	"github.com/coming-chat/go-aptos/aptostypes"
 )
 
-func (c *RestClient) GetEventsByKey(eventKey string) (res []aptostypes.Event, err error) {
-	req, err := http.NewRequest("GET", c.GetVersionedRpcUrl()+"/events/"+eventKey, nil)
+func (c *RestClient) GetEventsByEventHandle(address, eventHandle, field string, start, limit uint64) (res []aptostypes.Event, err error) {
+	req, err := http.NewRequest("GET", c.GetVersionedRpcUrl()+"/accounts/"+address+"/events/"+eventHandle+"/"+field, nil)
 	if err != nil {
 		return
 	}
+	q := req.URL.Query()
+	q.Add("start", strconv.FormatUint(start, 10))
+	q.Add("limit", strconv.FormatUint(limit, 10))
+	req.URL.RawQuery = q.Encode()
 	err = doReq(req, &res)
 	return
 }
 
-func (c *RestClient) GetEventsByEventHandle(address, eventHandle, field string, start, limit uint64) (res []aptostypes.Event, err error) {
-	req, err := http.NewRequest("GET", c.GetVersionedRpcUrl()+"/accounts/"+address+"/events/"+eventHandle+"/"+field, nil)
+func (c *RestClient) GetEventsByCreationNumber(address string, creationNumber string, start, limit uint64) (res []aptostypes.Event, err error) {
+	req, err := http.NewRequest("GET", c.GetVersionedRpcUrl()+"/accounts/"+address+"/events/"+creationNumber, nil)
 	if err != nil {
 		return
 	}
--- examples/rpc/main.go
+++ examples/rpc/main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"strconv"
 
 	"github.com/coming-chat/go-aptos/aptosclient"
 	"github.com/coming-chat/go-aptos/aptostypes"
@@ -38,111 +37,12 @@ func main() {
 		fmt.Printf("version: %d, type: %s, hash: %s, round: %d, time: %d\n", tx.Version, tx.Type, tx.Hash, tx.Round, tx.Timestamp)
 	}
 
-	account := "0x68fd7b5e581d2a95c5dbba09b9c19879c7a934f4f4cfda1d5008cc793660c8ee"
-	accountTransactions, err := client.GetAccountTransactions(account, 1, 10)
+	events, err := client.GetEventsByCreationNumber("0x8ab4fe552d76c49cdf7a4707338520ddf5de705044665fbcc4b6ea60a6b026d4", "2", 0, 10)
 	if err != nil {
 		printError(err)
 	}
-	printLine("get account tx list")
-	for _, tx := range accountTransactions {
-		fmt.Printf("type: %s, hash: %s, version: %d\n", tx.Type, tx.Hash, tx.Version)
-	}
-
-	txHash := "0xe1163f7b33df37995c9724b179b4d4a0fdff9eb2ef0c38a8d6e2982ce1c1de22"
-	tx, err := client.GetTransactionByHash(txHash)
-	if err != nil {
-		printError(err)
-	}
-	printLine("get tx by hash")
-	fmt.Printf("type: %s, hash: %s, version: %d\n", tx.Type, tx.Hash, tx.Version)
-
-	if tx.Type == aptostypes.TypeUserTransaction {
-		userTx := tx.AsUserTransaction()
-		fmt.Printf("type: %s, hash: %s, version: %d\n", userTx.Type, userTx.Hash, userTx.Version)
-	}
-
-	tx, err = client.GetTransactionByVersion(strconv.FormatUint(tx.Version, 10))
-	if err != nil {
-		printError(err)
-	}
-	printLine("get tx by version")
-	fmt.Printf("type: %s, hash: %s, version: %d\n", tx.Type, tx.Hash, tx.Version)
-
-	tx, err = client.GetTransactionByVersion(strconv.FormatUint(6618578, 10))
-	if err != nil {
-		printError(err)
-	}
-	if tx.Type == aptostypes.TypeBlockMetadataTransaction {
-		blockTx := tx.AsBlockMetadataTransaction()
-		fmt.Printf("type: %s, hash: %s, version: %d, id: %s\n", blockTx.Type, blockTx.Hash, blockTx.Version, blockTx.ID)
-	}
-
-	accountCore, err := client.GetAccount(account)
-	if err != nil {
-		printError(err)
-	}
-	fmt.Printf("seqNum: %d, key: %s\n", accountCore.SequenceNumber, accountCore.AuthenticationKey)
-
-	accountResources, err := client.GetAccountResources(account)
-	if err != nil {
-		printError(err)
-	}
-	printLine("account resource")
-	for _, resource := range accountResources {
-		fmt.Printf("resourceType: %s, data: %v\n", resource.Type, resource.Data)
-	}
-
-	resourceType := "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>"
-	resource, err := client.GetAccountResource(account, resourceType, 0)
-	if err != nil {
-		printError(err)
-	}
-	fmt.Printf("resourceType: %s\n, data: %v\n", resource.Type, resource.Data)
-
-	accountWithModule := "0xe72ca8ac40bb39bf4cf6d5abf8655015e3fce0f0a5e4218935355625eb0224a3"
-	accountModules, err := client.GetAccountModules(accountWithModule, 0)
-	if err != nil {
-		printError(err)
-	}
-	printLine("account modules")
-	for _, module := range accountModules {
-		fmt.Printf("abi:%s, name:%s\n", module.Abi.Address, module.Abi.Name)
-	}
-
-	accountModule, err := client.GetAccountModule(accountWithModule, "message", 0)
-	printLine("account module")
-	if err != nil {
-		printError(err)
-	} else {
-		fmt.Printf("abi:%s, name:%s\n", accountModule.Abi.Address, accountModule.Abi.Name)
-	}
-
-	eventKey := "0x0100000000000000874342f90ed0c0ccdf7baa13309820133ef94f143bb4a68069ceae8a8658541a"
-	events, err := client.GetEventsByKey(eventKey)
-	if err != nil {
-		printError(err)
-	}
-	printLine("events by key")
-	for _, event := range events {
-		fmt.Printf("key: %s, seqNum: %d, type: %s\n", event.Key, event.SequenceNumber, event.Type)
-	}
-
-	addressWithEvent := "0x647040d2018e65ae91a2353125a06a7b58917c523bef4e775237a814e464918c"
-	eventHandleStruct := "0x647040d2018e65ae91a2353125a06a7b58917c523bef4e775237a814e464918c::message::MessageHolder"
-	fieldName := "message_change_events"
-	events, err = client.GetEventsByEventHandle(addressWithEvent, eventHandleStruct, fieldName, 0, 0)
-	if err != nil {
-		printError(err)
-	}
-	printLine("events by address/handle/field")
-	for _, event := range events {
-		fmt.Printf("key: %s, seqNum: %d, type: %s\n", event.Key, event.SequenceNumber, event.Type)
-	}
-
-	printLine("error test")
-	_, err = client.GetEventsByEventHandle("0x123", eventHandleStruct, fieldName, 0, 0)
-	if err != nil {
-		printError(err)
+	for _, e := range events {
+		fmt.Println(e.SequenceNumber)
 	}
 }
 
