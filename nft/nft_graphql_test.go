376695bcca6e00ac7c51ef5866be69e0ac64a099
Gkirito <lownkid@outlook.com>
2022-11-03 12:02:09 +0800

    🧪 test(test): add filter test

--- nft/nft_graphql_test.go
+++ nft/nft_graphql_test.go
@@ -1,14 +1,24 @@
 package nft
 
-import "testing"
+import (
+	"github.com/coming-chat/go-aptos/graphql"
+	"testing"
+)
 
 func TestFetchGraphQL(t *testing.T) {
-	// owner := "0x6ed6f83f1891e02c00c58bf8172e3311c982b1c4fbb1be2d85a55562d4085fb1"
-	owner := "0xf5bb1482c28e3c600edf4cac9a10511b3d9a8e162d5b64d9741e2a8cb086bb50"
-	tokens, err := FetchGraphqlTokensOfOwner(owner, "")
+	owner := "0x559c26e61a74a1c40244212e768ab282a2cbe2ed679ad8421f7d5ebfb2b79fb5"
+	createrAddress := "0x01a97439554e302ee7584f55ea0035b6d4f131e09a024aba121139682a7ab3e8"
+	tokens, err := FetchGraphqlTokensOfOwner(owner, graphql.GraphUrlTestnet, "")
 	if err != nil {
 		t.Log(err)
 	} else {
 		t.Log(tokens)
+	}
+	t.Log("<===================test filter====================>")
+	tokensWithFilter, err := FetchGraphqlTokensOfOwner(owner, graphql.GraphUrlTestnet, createrAddress)
+	if err != nil {
+		t.Log(err)
+	} else {
+		t.Log(tokensWithFilter)
 	}
 }
